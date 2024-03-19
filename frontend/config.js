6d43556b56a124a11c417b73c68706998c7f6333
rajitShrestha <87907180+rajitShrestha@users.noreply.github.com>
2024-03-19 16:43:19 +0545

    Update config.js

--- frontend/config.js
+++ frontend/config.js
@@ -2,6 +2,6 @@
 
 // const API_BASE_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:5555';
 
-const API_BASE_URL = 'http://3.94.252.41:5555';
+const API_BASE_URL = 'http://100.65.141.171:5555';
 
 export default API_BASE_URL;
