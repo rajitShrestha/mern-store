60ebaa46c1c759517f2510950301f25f7d735847
Ubuntu <ubuntu@ip-172-31-33-76.ec2.internal>
2024-03-19 05:32:09 +0000

    added a docker-compose file

--- frontend/config.js
+++ frontend/config.js
@@ -1,6 +1,7 @@
 // config.js
-const API_BASE_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:5555';
 
-// const API_BASE_URL = 'http://192.168.33.10:5555';
+// const API_BASE_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:5555';
+
+const API_BASE_URL = 'http://3.94.252.41:5555';
 
 export default API_BASE_URL;
