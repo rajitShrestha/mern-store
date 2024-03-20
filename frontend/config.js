7724bcdb76fe5897a07b5c649c1fbcf2e0c81683
rajitShrestha <87907180+rajitShrestha@users.noreply.github.com>
2024-03-20 20:46:44 +0545

    Update config.js

--- frontend/config.js
+++ frontend/config.js
@@ -2,6 +2,6 @@
 
 // const API_BASE_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:5555';
 
-const API_BASE_URL = 'http://a5c6fb0f413504d9cab0a00ab48b24fd-2032794192.us-east-1.elb.amazonaws.com:5555';
+const API_BASE_URL = 'http://a084a8a6450d94d0ebb8d2dea3dddfac-1751116379.us-east-1.elb.amazonaws.com:5555';
 
 export default API_BASE_URL;
